d30703faf07be60a3dd2a22602357849c960fe55
titusfrezer <titofrezer2018@gmail.com>
2024-01-10 21:50:20 +0300

    done binary search list

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,21 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let low = 0;
+    let high = haystack.length;
+
+    do {
+        let middleIndex = Math.floor(low+(high-low)/2);
+        let middleValue = haystack[middleIndex];
+
+        if(middleValue === needle){
+            return true;
+        }
+        else if(middleValue>needle){
+            high = middleIndex;
+        }else{
+            low = middleIndex+1;
+        }
+    } while (low<high);
+
+    return false;
 }
