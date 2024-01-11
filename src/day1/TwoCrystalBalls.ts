a4b5db1f3845be79ba6a0d213a948d58428c4538
titusfrezer <titofrezer2018@gmail.com>
2024-01-11 11:55:41 +0300

    done two-crystal-bals

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,31 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+// worst way of doing it is using linear search
+    // for(let i=0;i<breaks.length;i++){
+    //     if(breaks[i]){
+    //         return i;
+    //     }
+    // }
+    // return -1;
+// best way of doing navigating with sqrt(breaks.length) 
 
+
+    let jumpAmount = Math.floor(Math.sqrt(breaks.length));
+    let i=jumpAmount;
+    for(;i<breaks.length;i+=jumpAmount){
+        if(breaks[i]){
+            break;
+        }
+    }
+
+    i -= jumpAmount;
+
+    // we used j<=jumpAmount if the value of the first breaking point is the best breaking point(in another words there is no breaking point below this point)
+    for(let j=0;j<=jumpAmount&&i<breaks.length;i++,j++){
+
+        if(breaks[i]){
+            return i;
+        }
+    }
+
+    return -1;
 }
